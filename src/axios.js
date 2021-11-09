8208c7495ea84bb692065d417ffa56882bd84218
Adithya Rajesh <52111169+DeltaCube23@users.noreply.github.com>
2021-11-09 12:14:35 +0400

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://rankedchoice34567.herokuapp.com/api/";
+const baseURL = "https://rankedchoice12345.herokuapp.com/api/";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
